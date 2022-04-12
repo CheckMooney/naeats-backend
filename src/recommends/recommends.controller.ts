81e23b936fc49f83e66a6e1a8b06d1ba60ec510a
sangrimlee <contact@sangrim.dev>
2022-04-12 10:08:04 +0900

    docs: 추천 관련 문서 추가
    - GET /recommends 문서 추가

--- src/recommends/recommends.controller.ts
+++ src/recommends/recommends.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ApiAuth } from 'src/docs/decorators';
 import { AuthUser } from 'src/auth/decorators/auth-user.decorator';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { User } from 'src/users/entities/user.entitiy';
 import { RecommendsService } from './recommends.service';
 import { GetRecommendsDto } from './dtos';
+import { GetRecommendsResponse } from './responses';
 
+@ApiTags('Recommends')
 @ApiAuth('AccessToken')
 @UseGuards(JwtAccessGuard)
 @Controller('recommends')
@@ -13,6 +16,11 @@ export class RecommendsController {
   constructor(private readonly recommendsService: RecommendsService) {}
 
   @Get()
+  @ApiOperation({ description: '음식 추천 리스트' })
+  @ApiResponse({
+    status: 200,
+    type: GetRecommendsResponse,
+  })
   async getRecommends(
     @AuthUser() user: User,
     @Query() getRecommendsDto: GetRecommendsDto,
