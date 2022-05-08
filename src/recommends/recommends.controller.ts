e57efb23f35bde22f5cfbc615cd4dd71f72be5ac
sangrimlee <contact@sangrim.dev>
2022-05-08 20:40:46 +0900

    docs: 최근 많이 먹은 음식 관련 문서 추가
    - GetRecentRecommendsResponse 추가

--- src/recommends/recommends.controller.ts
+++ src/recommends/recommends.controller.ts
@@ -6,7 +6,10 @@ import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { User } from 'src/users/entities/user.entitiy';
 import { RecommendsService } from './recommends.service';
 import { GetRecentRecommendsDto, GetRecommendsDto } from './dtos';
-import { GetRecommendsResponse } from './responses';
+import {
+  GetRecommendsResponse,
+  GetRecentRecommendsResponse,
+} from './responses';
 
 @ApiTags('Recommends')
 @ApiAuth('AccessToken')
@@ -36,6 +39,10 @@ export class RecommendsController {
 
   @Get('recent')
   @ApiOperation({ description: '최근 많은 사람들이 먹은 음식 추천 리스트' })
+  @ApiResponse({
+    status: 200,
+    type: GetRecentRecommendsResponse,
+  })
   async getRecentRecommends(
     @AuthUser() user: User,
     @Query() { hour = 1 }: GetRecentRecommendsDto,
@@ -44,7 +51,6 @@ export class RecommendsController {
       user.id,
       hour,
     );
-    console.log(hour);
     return {
       statusCode: 200,
       recommends,
--- src/recommends/responses/index.ts
+++ src/recommends/responses/index.ts
@@ -1,1 +1,2 @@
 export * from './get-recommends.response';
+export * from './get-recent-recommends.response';
